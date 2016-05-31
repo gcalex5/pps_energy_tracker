01003056b123893c44521eeea54dbf6957a850c8
Alex McCracken <alexmccracken55@gmail.com>
2016-05-31 18:07:17 -0400

    Additional progress on generic graphs algorithm
    Code formatting
    Cleaned up unused function
    worked on getting date into array key

--- src/Controller/Generic_Charts_Controller.php
+++ src/Controller/Generic_Charts_Controller.php
@@ -1,9 +1,5 @@
 <?php
 /**
- * 
- *
- *
- *
  * Created by PhpStorm.
  * User: Alex
  * Date: 5/30/2016
@@ -16,10 +12,6 @@ use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\Validator\Constraints\Null;
 
 class Generic_Charts_Controller {
-
-    //Class Wide Variables
-
-
     public $ON_PEAK_PRICES = array(array()); //On Peak Pricing Data
     public $OFF_PEAK_PRICES = array(array()); //Off Peak Pricing Data
     public $CHART_TYPE = 'NULL'; //Default Chart Type = ON PEAK
@@ -28,7 +20,7 @@ class Generic_Charts_Controller {
     public $PRICING_START = null; //Day that we will start the pricing on.
     public $MAX_DATE =  null; //Last day of pricing.
     public $ARRAY_DATE_KEYS = array(); //Need to change to 2D Array to handle multiple series
-    public $MONTHLY_USAGE = 100000;
+    public $MONTHLY_USAGE = 100000; //Declaration of the standard Monthly Usage as 100,000 kWh
     /**
      * ->Entry Point Of Class<-
      * Makes the initial function call to start calculating the pricing data.
@@ -46,14 +38,10 @@ class Generic_Charts_Controller {
 
         //Grab the required data out of the on and off peak tables
         $pricingHolder = $this->queryData($graph_choice, $graph_type);
-
         $this->septemberFix();
 
-        //Sort out the data into usable arrays
-        //TODO: Remove this if it is no longer necessary
-        //$priceArrays = $this->createDataArray($pricingHolder);
-
         //Calculate the price points to be graphed.
+        $this->PRICING_START = new \DateTime('2013-03-01');
         $pricing = $this->pricingGeneration($pricingHolder);
         return $pricing;
     }
@@ -96,110 +84,86 @@ class Generic_Charts_Controller {
     }
 
     /**
-     * Sort out the queried data into usable arrays and return it to the controller
-     *
-     * Potentially move this into an entity class object?
-     *
-     * @param $temp_array - Passed in array containing all of the usable data
-     * @return array
-     */
-    //TODO: Remove this is most likely not necessary anymore. Handled via using ->fetchAllAssoc('purchase_date')
-    public function createDataArray($temp_array){
-        $on_peak = array(array());
-        $off_peak = array(array());
-        for($i=0; $i<2; $i++){
-            for($j=0; $j<sizeof($temp_array[$i]); $j++){
-                if($i==0){
-                    $on_peak[$temp_array[$i][$j]->purchase_date][] = $temp_array[$i][$j];
-                }
-                else{
-                    $off_peak[$temp_array[$i][$j]->purchase_date][] = $temp_array[$i][$j];
-                }
-            }
-        }
-        return [$on_peak, $off_peak];
-    }
-
-    /**
      * Generates the requested pricing points and returns them to the view
      *
      * @param $data_array - queried array returned from queryData
      * @return string - placeholder return documentation
      */
     //TODO: Add an argument to let this function know how many series we need to generate
-    public function pricingGeneration($data_array){
-
-        //Define the pricing constants that control the do/while loop.
-        //TODO: Swap this over to user input once it is functional
-        $PRICING_START = new \DateTime('2013-03-01');
-        $MAX_DATE = new \DateTime('2016-05-16');
-
-        //Data array holding the date/price array
-        $pricing_array = array(array());
+  public function pricingGeneration($data_array){
+    //Define the pricing constants that control the do/while loop.
+    //TODO: Swap this over to user input once it is functional
+    $temp_array = array(array());
+    $pricing_array = array(array());
+    $monthly_on_total = 0;
+    $monthly_off_total = 0;
+    //Run the loop one to three times depending upon the request
+    //TODO: Swap this to run up to three times depending on the graph type. Maybe IF(GRAPH TYPE)->FOR LOOP
+    for($i=0; $i<1; $i++){
+        //do/while loop runs from PRICING_START until MAX_DATE incrementing one day at a time
+        $zero_counter = 0;
+        $current_keys = array_keys($data_array[$i]);
+        $this->MAX_DATE = new \DateTime($current_keys[sizeof($current_keys) - 3]);
+      do{
+        for($j=0; $j<12; $j++){
+          $current_date_key = $this->ARRAY_DATE_KEYS[$j];
+          $current_price = $data_array[$i][$this->PRICING_START->format('Y-m-d')]->$current_date_key;
+          if($current_price > 0.0001){
+            $monthly_on_total += (($current_price / 1000) * $this->MONTHLY_USAGE);
+          }
+          else{
+            $zero_counter++;
+          }
+        }
+        /**
+         * Calculate the price for today and add it to the pricing list
+         *
+         * Price = (Term Cost / (Term Vol - (Num Zeroes * 100K ))
+         */
+        if($zero_counter != 12){
+          $price = ($monthly_on_total + $monthly_off_total) / (($this->MONTHLY_USAGE * 12) - ($zero_counter * $this->MONTHLY_USAGE));
+          $temp_array[$this->PRICING_START->format('Y-m-d')][] = $price;
+        }
+        else{
+          //We have 12 0's nothing is necessary
+        }
+        //Flush variables
+        //TODO: Flush variables?
         $monthly_on_total = 0;
+        $zero_counter = 0;
         $monthly_off_total = 0;
-
-        //Run the loop one to three times depending upon the request
-        //TODO: Swap this to run up to three times depending on the graph type. Maybe IF(GRAPH TYPE)->FOR LOOP
-        for($i=0; $i<1; $i++){
-            //do/while loop runs from PRICING_START until MAX_DATE incrementing one day at a time
-            $zero_counter = 0;
-            do{
-                //For loop controlling the on peak pricing
-                for($j=0; $j<12; $j++){
-                    $current_key = $this->ARRAY_DATE_KEYS[$j];
-                    $current_price = $data_array[$i][$this->PRICING_START->format('Y-m-d')]->$current_key;
-                    if($current_price > 0.0001){
-                        $monthly_on_total += ($current_price / 1000) * $this->MONTHLY_USAGE;
-                    }
-                    else{
-                        $zero_counter++;
-                    }
-                }
-                //For loop controlling the off peak pricing
-
-                //For loop controlling mixed pricing
-
-                /**
-                 * Calculate the price for today and add it to the pricing list
-                 *
-                 * Price = (Term Cost / (Term Vol - (Num Zeroes * 100K ))
-                 */
-                $price = ($monthly_on_total + $monthly_off_total) / (($this->MONTHLY_USAGE * 12) - ($zero_counter * $this->MONTHLY_USAGE));
-                $pricing_array[$i][$this->PRICING_START] = $price;
-                $PRICING_START->add(new \DateInterval('P1D'));
-                //TODO: Flush variables?
-            }while($PRICING_START < $MAX_DATE);
-        }
-        return $pricing_array;
+        $this->PRICING_START->add(new \DateInterval('P1D'));
+      }while($this->PRICING_START < $this->MAX_DATE);
     }
+    return $pricing_array;
+  }
 
-    /**
-     * Utility Function tests the start date to see if it will need moved.
-     * Move Scenario: Current start date has no meaningful data
-     *
-     * Calls moveStartDate if a 'Move Scenario' is met
-     *
-     * @param $current_start - the date that we are looking to start pricing.
-     */
-    public function testStartData($current_start){
+  /**
+   * Utility Function tests the start date to see if it will need moved.
+   * Move Scenario: Current start date has no meaningful data
+   *
+   * Calls moveStartDate if a 'Move Scenario' is met
+   *
+   * @param $current_start - the date that we are looking to start pricing.
+   */
+  public function testStartData($current_start){
 
-    }
+  }
 
-    /**
-     * Utility function called from testStartDate slides the current start date forward by a day
-     *
-     * Calls back to testStartDate with the moved date
-     *
-     * @param $current_start - current starting date passed from testStartDate
-     */
-    public function moveStartDate($current_start){
+  /**
+   * Utility function called from testStartDate slides the current start date forward by a day
+   *
+   * Calls back to testStartDate with the moved date
+   *
+   * @param $current_start - current starting date passed from testStartDate
+   */
+  public function moveStartDate($current_start){
 
-    }
+  }
 
-    public function septemberFix(){
-        for($i=0; $i<sizeof($this->ARRAY_DATE_KEYS); $i++){
-            $this->ARRAY_DATE_KEYS[$i] = str_replace('Sep', 'Sept', $this->ARRAY_DATE_KEYS[$i]);
-        }
-    }
+  public function septemberFix(){
+      for($i=0; $i<sizeof($this->ARRAY_DATE_KEYS); $i++){
+          $this->ARRAY_DATE_KEYS[$i] = str_replace('Sep', 'Sept', $this->ARRAY_DATE_KEYS[$i]);
+      }
+  }
 }
