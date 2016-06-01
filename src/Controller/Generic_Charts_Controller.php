a100f16a76e7402c9a6f3b11a41dacfa34e95672
Alex McCracken <alexmccracken55@gmail.com>
2016-06-01 08:52:45 -0400

    Code formatting changes
    Changed indentation

--- src/Controller/Generic_Charts_Controller.php
+++ src/Controller/Generic_Charts_Controller.php
@@ -12,84 +12,84 @@ use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\Validator\Constraints\Null;
 
 class Generic_Charts_Controller {
-    public $ON_PEAK_PRICES = array(array()); //On Peak Pricing Data
-    public $OFF_PEAK_PRICES = array(array()); //Off Peak Pricing Data
-    public $CHART_TYPE = 'NULL'; //Default Chart Type = ON PEAK
-    public $TERM_START = null; //When the term starts for pricing
-    public $TERM_END = null; //When the term ends for pricing
-    public $PRICING_START = null; //Day that we will start the pricing on.
-    public $MAX_DATE =  null; //Last day of pricing.
-    public $ARRAY_DATE_KEYS = array(); //Need to change to 2D Array to handle multiple series
-    public $MONTHLY_USAGE = 100000; //Declaration of the standard Monthly Usage as 100,000 kWh
-    /**
-     * ->Entry Point Of Class<-
-     * Makes the initial function call to start calculating the pricing data.
-     *
-     * @param $graph_choice - graph that needs to be queried Years 2015/2016/2017/2018/etc.
-     * @param $graph_type - the type of graph that needs drawn On/Off/Mixed
-     * @return string - return the pricing array
-     */
-    public function pricingController($graph_choice, $graph_type){
-        //TODO: Set all constants before we move forward need to be moved into dynamic user input
-        $this->PRICING_START = new \DateTime('2013-03-01');
-        $this->TERM_START = new \DateTime('2017-01-01');
-        $this->TERM_END = new \DateTime('2017-12-31');
-        $this->CHART_TYPE = $graph_type;
+  public $ON_PEAK_PRICES = array(array()); //On Peak Pricing Data
+  public $OFF_PEAK_PRICES = array(array()); //Off Peak Pricing Data
+  public $CHART_TYPE = 'NULL'; //Default Chart Type = ON PEAK
+  public $TERM_START = null; //When the term starts for pricing
+  public $TERM_END = null; //When the term ends for pricing
+  public $PRICING_START = null; //Day that we will start the pricing on.
+  public $MAX_DATE =  null; //Last day of pricing.
+  public $ARRAY_DATE_KEYS = array(); //Need to change to 2D Array to handle multiple series
+  public $MONTHLY_USAGE = 100000; //Declaration of the standard Monthly Usage as 100,000 kWh
+  /**
+   * ->Entry Point Of Class<-
+   * Makes the initial function call to start calculating the pricing data.
+   *
+   * @param $graph_choice - graph that needs to be queried Years 2015/2016/2017/2018/etc.
+   * @param $graph_type - the type of graph that needs drawn On/Off/Mixed
+   * @return string - return the pricing array
+   */
+  public function pricingController($graph_choice, $graph_type){
+      //TODO: Set all constants before we move forward need to be moved into dynamic user input
+      $this->PRICING_START = new \DateTime('2013-03-01');
+      $this->TERM_START = new \DateTime('2017-01-01');
+      $this->TERM_END = new \DateTime('2017-12-31');
+      $this->CHART_TYPE = $graph_type;
 
-        //Grab the required data out of the on and off peak tables
-        $pricingHolder = $this->queryData($graph_choice, $graph_type);
-        $this->septemberFix();
+      //Grab the required data out of the on and off peak tables
+      $pricingHolder = $this->queryData($graph_choice, $graph_type);
+      $this->septemberFix();
 
-        //Calculate the price points to be graphed.
-        $this->PRICING_START = new \DateTime('2013-03-01');
-        $pricing = $this->pricingGeneration($pricingHolder);
-        return $pricing;
-    }
+      //Calculate the price points to be graphed.
+      $this->PRICING_START = new \DateTime('2013-03-01');
+      $pricing = $this->pricingGeneration($pricingHolder);
+      return $pricing;
+  }
 
-    /**
-     * Takes in the graph choice, and generates a query returning the data to the controller
-     *
-     * @param $graph_choice - graph that needs to be queried Years 2015/2016/2017/2018/etc.
-     * @param $graph_type - the type of graph that needs drawn On/Off/Mixed
-     * @return array - Return an array containing the queried data 0 - On Peak 1 - Off Peak
-     */
-    public function queryData($graph_choice, $graph_type){
-        //TODO: Change queries to be dependent on user input
-        $query = 'Select purchase_date, ';
-        do{
-            $query = $query . $this->TERM_START->format('M_y, ');
-            //TODO: Change to 2D Array to handle multiple series
-            $this->ARRAY_DATE_KEYS[] = $this->TERM_START->format('M_y');
-            $this->TERM_START->add(new \DateInterval('P1M'));
-        }while($this->TERM_START < $this->TERM_END);
+  /**
+   * Takes in the graph choice, and generates a query returning the data to the controller
+   *
+   * @param $graph_choice - graph that needs to be queried Years 2015/2016/2017/2018/etc.
+   * @param $graph_type - the type of graph that needs drawn On/Off/Mixed
+   * @return array - Return an array containing the queried data 0 - On Peak 1 - Off Peak
+   */
+  public function queryData($graph_choice, $graph_type){
+      //TODO: Change queries to be dependent on user input
+      $query = 'Select purchase_date, ';
+      do{
+          $query = $query . $this->TERM_START->format('M_y, ');
+          //TODO: Change to 2D Array to handle multiple series
+          $this->ARRAY_DATE_KEYS[] = $this->TERM_START->format('M_y');
+          $this->TERM_START->add(new \DateInterval('P1M'));
+      }while($this->TERM_START < $this->TERM_END);
 
-        //Reset TERM_START
-        //TODO: Change to user input
-        $this->TERM_START = new \DateTime('2017-01-01');
+      //Reset TERM_START
+      //TODO: Change to user input
+      $this->TERM_START = new \DateTime('2017-01-01');
 
-        //Remove the last comma
-        $query = preg_replace('/,([^,]*)$/', ' \1', $query);
+      //Remove the last comma
+      $query = preg_replace('/,([^,]*)$/', ' \1', $query);
 
-        //Swap September's abbreviation to match the data set and query on peak numbers
-        //TODO: Change this to only grab the on peak data if necessary
-        $query = str_replace('Sep', 'Sept', $query);
-        $query.= "FROM ppsweb_pricemodel.elec_on_peak WHERE purchase_date >= '". $this->PRICING_START->format('Y-m-d') . "' ORDER BY purchase_date";
-        $on_data = db_query($query)->fetchAllAssoc('purchase_date');
+      //Swap September's abbreviation to match the data set and query on peak numbers
+      //TODO: Change this to only grab the on peak data if necessary
+      $query = str_replace('Sep', 'Sept', $query);
+      $query.= "FROM ppsweb_pricemodel.elec_on_peak WHERE purchase_date >= '". $this->PRICING_START->format('Y-m-d') . "' ORDER BY purchase_date";
+      $on_data = db_query($query)->fetchAllAssoc('purchase_date');
 
-        //modify the query string to grab the off peak numbers.
-        $query = str_replace('elec_on_peak', 'elec_off_peak', $query);
-        $off_data = db_query($query)->fetchAllAssoc('purchase_date');
+      //modify the query string to grab the off peak numbers.
+      $query = str_replace('elec_on_peak', 'elec_off_peak', $query);
+      $off_data = db_query($query)->fetchAllAssoc('purchase_date');
 
-        return [$on_data, $off_data];
-    }
+      return [$on_data, $off_data];
+  }
 
-    /**
-     * Generates the requested pricing points and returns them to the view
-     *
-     * @param $data_array - queried array returned from queryData
-     * @return string - placeholder return documentation
-     */
-    //TODO: Add an argument to let this function know how many series we need to generate
+  /**
+   * Generates the requested pricing points and returns them to the view
+   *
+   * @param $data_array - queried array returned from queryData
+   * @return string - placeholder return documentation
+   */
+  //TODO: Add an argument to let this function know how many series we need to generate
   public function pricingGeneration($data_array){
     //Define the pricing constants that control the do/while loop.
     //TODO: Swap this over to user input once it is functional
